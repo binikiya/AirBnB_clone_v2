02725689193b8be0b5ff70004704bf13c9442051
binikiya <biniyamkiya3682@gmail.com>
2023-03-21 05:09:15 -0700

    AirBnB Project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
---managing a database for a project
+-- prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
