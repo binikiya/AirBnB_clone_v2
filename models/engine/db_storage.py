94cd23756553efe45403c55f4982424cfeb74273
binikiya <biniyamkiya3682@gmail.com>
2023-03-21 05:04:35 -0700

    AirBnB Project

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """create class DBStorage"""
-from models.base_model import BaseModel, Base
-from models.state import State
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
 from os import getenv
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, scoped_session
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 database = getenv("HBNB_MYSQL_DB")
 user = getenv("HBNB_MYSQL_USER")
@@ -21,13 +22,13 @@ classes = {"State": State, "City": City, "User": User,
            "Place": Place, "Review": Review, "Amenity": Amenity}
 
 
-
 class DBStorage:
     """class DBStorage"""
     __engine = None
     __session = None
 
     def __init__(self):
+        """initialize instances"""
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format
                                       (user, password, host, database),
                                       pool_pre_ping=True)
@@ -36,7 +37,12 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """returns dictionary of instance attributes"""
+        """return dictionary of instance attributes
+        Args:
+            cls (obj): memory address of class
+        Returns:
+            dictionary of objects
+        """
         dbobjects = {}
         if cls:
             if type(cls) is str and cls in classes:
@@ -58,25 +64,38 @@ class DBStorage:
         return dbobjects
 
     def new(self, obj):
-        """add an object to the current database session"""
+        """
+        add object to current database session
+        Args:
+            obj (obj): an object
+        """
         if obj:
             self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """
+        commit all changes of the current database session
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """deletes from the current database sission"""
+        """
+        delete from the current database session obj if not None
+        Args:
+            obj (obj): an object
+        """
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """
+        create all tables in the database and the current database session
+        """
         Base.metadata.create_all(self.__engine)
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sess_factory)
-        self.__session = Session
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
 
     def close(self):
         """close session"""
