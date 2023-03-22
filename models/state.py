43ee582b5de6b6383fb58eeb3af1d0f6d5220c84
binikiya <biniyamkiya3682@gmail.com>
2023-03-21 23:33:56 -0700

    AirBnB Project

--- models/state.py
+++ models/state.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-import models
-from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
-
-    @property
-    def cities(self):
-        """get list of City instances with state_id = current State.id"""
-        list_cities = []
-        all_cities = models.storage.all(City)
-        for key, city_obj in all_cities.items():
-            if city_obj.state_id == self.id:
-                list_cities.append(city_obj)
-        return list_cities
+    name = ""
