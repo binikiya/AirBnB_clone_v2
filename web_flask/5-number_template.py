5529275a4b0e905c09d976999f561a6f0a6aac0a
Biniyam <biniyamkiya3682@gmail.com>
2023-04-21 15:36:21 +0300

    Airbnb Web Framework

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def only_integer(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def only_integer_html(n):
     """displaying in html"""
-    return render_template("5-number.html", n = n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
