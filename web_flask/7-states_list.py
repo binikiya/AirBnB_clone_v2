ee5a59c5c0f93815fafedc2c76b01c9398de99bf
Biniyam <biniyamkiya3682@gmail.com>
2023-04-21 16:50:37 +0300

    Airbnb Web Framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 """Flask class"""
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
-
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
 @app.teardown_appcontext
-def tear_down(self):
-    """closint storage"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    """display an html page"""
-    dict_states = storage.all(State)
-    all_states = []
-    for k, v in dict_states.items():
-        all_states.append(v)
-    return render_template('7-states_list.html', all_states=all_states)
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
