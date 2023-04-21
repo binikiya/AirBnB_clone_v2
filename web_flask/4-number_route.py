061f4bd12c0dcefe67b15644ffa843344bea06ec
Biniyam <biniyamkiya3682@gmail.com>
2023-04-21 14:40:18 +0300

    Airbnb Web Framework

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Flask class"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """displaying"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_only():
+    """displaying"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """displaying"""
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """displaying"""
+    return "Python %s" % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def only_integer(n):
+    """displaying only int"""
+    return "%d is a number" % n
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
